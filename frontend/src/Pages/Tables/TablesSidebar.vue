7539c66f9dfbca6e3c06fd80ce46450dcc46472d
iDevelopThings <sam@idevelopthings.com>
2023-05-23 18:14:49 +0100

    Add overflow-y-auto to TablesSidebar.vue closes #2

--- frontend/src/Pages/Tables/TablesSidebar.vue
+++ frontend/src/Pages/Tables/TablesSidebar.vue
@@ -1,15 +1,15 @@
 <template>
-	<div class="w-full max-w-[13em] bg-main-800 divide-y divide-slate-600">
-		<div class="flex flex-col  justify-center">
-			<p class="py-3 px-6 text-lg">Tables</p>
-		</div>
-		<div class="flex flex-col divide-y divide-slate-600">
+	<div class="w-full max-w-[13em] bg-main-800 divide-y divide-slate-600 overflow-y-auto">
+    <div class="flex flex-col  justify-center">
+      <p class="py-3 px-6 text-lg">Tables</p>
+    </div>
+    <div class="flex flex-col divide-y divide-slate-600">
 
-			<template v-if="schemaStore.isLoading">
-				<Spinner />
-			</template>
-			<template v-else>
-				<!--					@click.native="tableStore.load(table)"-->
+      <template v-if="schemaStore.isLoading">
+        <Spinner/>
+      </template>
+      <template v-else>
+        <!--					@click.native="tableStore.load(table)"-->
 				<TableSidebarItem
 					:title="table.title"
 					:to="{name: 'tables.view', params: {name: table.name}}"
