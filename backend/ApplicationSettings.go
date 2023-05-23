848f6b8b51fcca3d85326ea47a6738a330bb878d
iDevelopThings <sam@idevelopthings.com>
2023-05-23 18:16:44 +0100

    Increment app version

--- backend/ApplicationSettings.go
+++ backend/ApplicationSettings.go
@@ -24,7 +24,7 @@ func NewApplicationSettings() *ApplicationSettings {
 	settings := &ApplicationSettings{
 		Title:   "SurrealDB Explorer",
 		Name:    "surrealdb_explorer",
-		Version: semver.MustParse("0.0.12"),
+		Version: semver.MustParse("0.0.13"),
 	}
 
 	FileStore.NewStores(settings.Name)
